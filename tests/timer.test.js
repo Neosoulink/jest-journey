b09c2618e8a75831bca5c13fdd009cd836f0c397
Neosoulink <neodarksoulink@gmail.com>
2021-11-25 14:24:00 +0100

    Concurante notion

--- tests/timer.test.js
+++ tests/timer.test.js
@@ -1,8 +1,17 @@
 import { sleep } from "../src/timer";
+describe('Timer', ()=> {
+	it.concurrent("Should sleep for 3s", async () => {
+		const t = Date.now();
+		await sleep(2);
 
-test("Should sleep for 3s", async () => {
-	const t = Date.now();
-	await sleep(3);
+		expect(Date.now() - t).toBeGreaterThanOrEqual(2000);
+	});
 
-	expect(Date.now() - t).toBeGreaterThanOrEqual(2000);
+	it.concurrent("Should sleep for 1s", async () => {
+		const t = Date.now();
+		await sleep(1);
+
+		expect(Date.now() - t).toBeGreaterThanOrEqual(1000);
+	});
+
 });
